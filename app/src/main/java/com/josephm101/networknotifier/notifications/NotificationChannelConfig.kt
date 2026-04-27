e623dac74c4ed3647f06e19657100c2a27c1d5c2
Joseph Mignone <jmig3066@gmail.com>
2026-04-27 11:23:41 -0400

    Delete unused classes

--- app/src/main/java/com/josephm101/networknotifier/notifications/NotificationChannelConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.josephm101.networknotifier.notifications
-
-data class NotificationChannelConfig(
-    var channelId: String,
-    var channelDisplayName: String,
-    var channelDescription: String,
-)
--- app/src/main/java/com/josephm101/networknotifier/service/NotificationConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.josephm101.networknotifier.service
-
-import com.josephm101.networknotifier.notifications.NotificationChannelConfig
-
-data class NotificationConfig(
-    var notificationId: Int,
-    var notificationChannelConfig: NotificationChannelConfig,
-)
